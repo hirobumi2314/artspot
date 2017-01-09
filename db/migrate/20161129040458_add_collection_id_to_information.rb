7f19ce197e021a31303103f6e8f57f8c9cb0a82f
Kenji Tanaka <kenji5621@outlook.com>
2017-01-09 01:45:35 -0500

    Change DB from mysql to psql

--- db/migrate/20161129040458_add_collection_id_to_information.rb
+++
@@ -1,5 +0,0 @@
-class AddCollectionIdToInformation < ActiveRecord::Migration
-  def change
-  	add_index :information, :collection_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170109050725) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "artbases", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -20,30 +23,6 @@ ActiveRecord::Schema.define(version: 20170109050725) do
 
   create_table "collections", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "comments", force: true do |t|
-    t.integer  "collection_id"
-    t.text     "maintitle"
-    t.text     "subtitle"
-    t.text     "year"
-    t.text     "medium"
-    t.text     "duration"
-    t.text     "objectnumber"
-    t.text     "production"
-    t.text     "department"
-    t.text     "publisher"
-    t.text     "printer"
-    t.text     "edition"
-    t.text     "copyright"
-    t.text     "portfolio"
-    t.text     "architectural"
-    t.text     "dimentions"
-    t.text     "credit"
-    t.text     "mtype"
-    t.text     "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
